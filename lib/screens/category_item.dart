c6f9c1fbb0c86bc69ad67eb965449b0c64a9543b
DeleMike <akindelemichael65@gmail.com>
2020-11-26 15:41:40 +0100

    add ontap event on category item

--- lib/screens/category_item.dart
+++ lib/screens/category_item.dart
@@ -1,27 +1,42 @@
+import 'package:Meals/screens/category_meals.dart';
 import 'package:flutter/material.dart';
+
 
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
   CategoryItem({this.title, this.color});
+
+  void selectCategory(BuildContext context){
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+      return CategoryMeals();
+    }),);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15.0),
-      child: Text(
-        title,
-      ),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-        colors: [
-          color.withOpacity(0.7),
-          color,
-        ],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => selectCategory(context) ,
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+          child: Container(
+        padding: const EdgeInsets.all(15.0),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
         ),
-        borderRadius: BorderRadius.circular(15),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          colors: [
+            color.withOpacity(0.7),
+            color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
