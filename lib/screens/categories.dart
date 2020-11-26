cba540a9c2635f06537a6f9816be042da348afba
DeleMike <akindelemichael65@gmail.com>
2020-11-26 13:30:01 +0100

    map dummy data to gridview

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,10 +1,19 @@
+import 'package:Meals/models/category.dart';
 import 'package:flutter/material.dart';
+
+import 'package:Meals/dummy_data.dart';
+import 'package:Meals/screens/category_item.dart';
 
 class Categories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: [],
+      children: DUMMY_CATEGORIES.map((categoryData) {
+        return CategoryItem(
+          title: categoryData.title,
+          color: categoryData.color,
+        );
+      }).toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200.0,
         childAspectRatio: 3 / 2,
