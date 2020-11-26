ac79c81026d4770e523d8861f698b3dcc0b6d342
DeleMike <akindelemichael65@gmail.com>
2020-11-26 13:37:33 +0100

    added scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './screens/categories.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -14,29 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Meals'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text('Navigation'),
-      ),
+      home: Categories(),
     );
   }
 }
--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,24 +1,26 @@
-import 'package:Meals/models/category.dart';
 import 'package:flutter/material.dart';
 
-import 'package:Meals/dummy_data.dart';
-import 'package:Meals/screens/category_item.dart';
+import '../dummy_data.dart';
+import '../screens/category_item.dart';
 
 class Categories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES.map((categoryData) {
-        return CategoryItem(
-          title: categoryData.title,
-          color: categoryData.color,
-        );
-      }).toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200.0,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(title: const Text('Meals'),),
+          body: GridView(
+        children: DUMMY_CATEGORIES.map((categoryData) {
+          return CategoryItem(
+            title: categoryData.title,
+            color: categoryData.color,
+          );
+        }).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200.0,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
