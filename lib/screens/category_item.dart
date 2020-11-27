b981caaf5ec7c032bd65bb2d4cadf6c433da5074
DeleMike <akindelemichael65@gmail.com>
2020-11-27 11:16:27 +0100

    use named routes for navigation

--- lib/screens/category_item.dart
+++ lib/screens/category_item.dart
@@ -1,39 +1,41 @@
 import 'package:Meals/screens/category_meals.dart';
 import 'package:flutter/material.dart';
 
-
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem({this.title, this.color});
+  CategoryItem({this.id, this.title, this.color});
 
-  void selectCategory(BuildContext context){
-    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
-      return CategoryMeals();
-    }),);
+  void selectCategory(BuildContext context) {
+    //how to pass data when using named routes without the constructor.
+    Navigator.of(context).pushNamed(CategoryMeals.routeName , arguments: {
+      'id': id,
+      'title': title,
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () => selectCategory(context) ,
+      onTap: () => selectCategory(context),
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(15),
-          child: Container(
+      child: Container(
         padding: const EdgeInsets.all(15.0),
         child: Text(
           title,
           style: Theme.of(context).textTheme.headline6,
         ),
         decoration: BoxDecoration(
-            gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
           borderRadius: BorderRadius.circular(15),
         ),
